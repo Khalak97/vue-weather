519312ea56aa5d9cef54da583b7449c2ca8d892e
Khalak97 <valodyakh@gmail.com>
2020-07-07 12:18:07 +0300

    add location

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,10 @@
                 borderless
             >
                 <template v-slot:before>
-                    <q-icon name="my_location" />
+                    <q-icon
+                        @click="getLocation"
+                        name="my_location"
+                    />
                 </template>
 
                 <template v-slot:append>
@@ -17,17 +20,36 @@
             </q-input>
         </div>
 
-        <div class="col text-white text-center">
-            <div class="text-h4 text-weight-light">
-                Ukraine
+        <template v-if="weatherData">
+            <div class="col text-white text-center">
+                <div class="text-h4 text-weight-light">
+                    Ukraine
+                </div>
+                <div class="text-h6 text-weight-light">
+                    Rain
+                </div>
+                <div class="text-h1 text-weight-thin">
+                    22&deg;
+                </div>
             </div>
-            <div class="text-h6 text-weight-light">
-                Rain
+        </template>
+
+        <template v-else>
+            <div class="col column text-center text-white">
+                <div class="col text-h2 text-weight-thin">
+                    Quasar<br>Weather
+                </div>
+
+                <q-btn
+                    @click="getLocation"
+                    color="col"
+                    flat
+                >
+                    <q-icon left size="3em" name="my_location" />
+                    <div>Find my location</div>
+                </q-btn>
             </div>
-            <div class="text-h1 text-weight-thin">
-                22&deg;
-            </div>
-        </div>
+        </template>
     </q-page>
 </template>
 
@@ -36,7 +58,28 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      search: ''
+      search: '',
+      weatherData: null,
+      lat: null,
+      lon: null,
+      apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+      apiKey: ''
+    }
+  },
+  methods: {
+    getLocation () {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log('position: ', position)
+        this.lat = position.coords.latitude
+        this.lon = position.coords.latitude
+        this.getWeatherByCoords()
+      })
+    },
+    getWeatherByCoords () {
+      this.axious(`${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`)
+        .then(response => {
+          this.weatherData = response.data
+        })
     }
   }
 }
